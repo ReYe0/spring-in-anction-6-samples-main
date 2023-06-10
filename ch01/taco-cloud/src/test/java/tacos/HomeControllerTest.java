f6c2579b8f90e10b56581fe0ce4bf2d3f504c054
Xuyong <1299461580@qq.com>
2023-06-11 01:27:08 +0800

    第一章 MVC 测试

--- ch01/taco-cloud/src/test/java/tacos/HomeControllerTest.java
+++ ch01/taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -11,18 +11,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(HomeController.class)   // <1>
+@WebMvcTest(HomeController.class)   // <1> //针对 HomeController 的web测试类。这个注解会将 bean 注册到 Spring MVC 上。
 public class HomeControllerTest {
 
   @Autowired
-  private MockMvc mockMvc;   // <2>
+  private MockMvc mockMvc;   // <2> 注入 MockMVC
 
   @Test
   public void testHomePage() throws Exception {
-    mockMvc.perform(get("/"))    // <3>
-      .andExpect(status().isOk())  // <4>
-      .andExpect(view().name("home"))  // <5>
-      .andExpect(content().string(           // <6>
+    mockMvc.perform(get("/"))    // <3> 发起对”/"的 GET 请求
+      .andExpect(status().isOk())  // <4> 期望得到 HTTP 200
+      .andExpect(view().name("home"))  // <5> 期望得到 home 视图
+      .andExpect(content().string(           // <6> 期望宝行“Welcome to……”
           containsString("Welcome to...")));
   }
 
